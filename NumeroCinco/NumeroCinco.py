eec34e69f41e4b14411827881c88479e15afebdc
Ethan Silvera <edsilverado2004@gmail.com>
2023-01-26 09:54:30 -0600

    Project Done

--- NumeroCinco/NumeroCinco.py
+++ NumeroCinco/NumeroCinco.py
@@ -13,9 +13,12 @@ for i in range(0,len(names)):
     for a in range(0, len(names[i])):
         for letter in names[i]:
             word.append(letter)
+        #Each letter coresponds to a number (Its position in the alphabet)
         for x in range(0,len(alpha)):
             if(word[a] == alpha[x]):
+                #Subtotaling the score of each word
                 index = index + (x+1)
+                #Multiply each word's score by its position in the names list
                 sub = i*index
     index=0
     total = total+sub
